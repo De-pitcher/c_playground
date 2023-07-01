8d7ea9071e029aaf9d77ee8301d3df9bc66c1502
De-pitcher <emmanwa000@gmail.com>
2023-07-01 16:38:32 -0700

    Added mul program

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ * @a: first int
+ * @b: second int
+ *
+ * Return: producto of a and b.
+ */
+
+ int mul(int a, int b)
+ {
+	 return (a * b);
+ }
