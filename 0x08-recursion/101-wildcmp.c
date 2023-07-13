d6228179efa17aeb4babd307c8ddf9fbb0f028bb
Noel Emmanuel <emmanwa000@gmail.com>
2023-07-12 22:02:26 -0700

    added the the wildcmp program

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * wildcmp - compares two strings and checks if hey are identical.
+ * @s1: string 1.
+ * @s2: strin2.
+ *
+ * Return: 1 if true else 0.
+ */
+
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0')
+	{
+		if (*s2 != '\0' && *s2 == '*')
+		{
+			return (wildcmp(s1, s2 + 1));
+		}
+		return (*s2 == '\0');
+	}
+	if (*s2 == '*')
+	{
+		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+	}
+	else if (*s1 == *s2)
+	{
+		return (wildcmp(s1 + 1, s2 + 1));
+	}
+	return (0);
+}
